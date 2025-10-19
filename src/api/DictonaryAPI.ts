4568fbdebd4b79b4afc4adf04885cff062f85a0d
ben <ben.egg.gov@gmail.com>
2025-10-19 20:24:18 +0300

    added type

--- src/api/DictonaryAPI.ts
+++ src/api/DictonaryAPI.ts
@@ -1,17 +1,27 @@
 const url = "https://freedictionaryapi.com/api/v1/entries/{language}/{word}"
 
-export const wordExists = async (language: string = "en", word: string): Promise<boolean> => {
-    return await getWordData(language, word) != undefined
+export interface WordData {
+    word: string,
+    entries: object[]
 }
 
-export const getWordData = async (language: string = "en", word: string): Promise<object | undefined> => {
+export const wordExists = async (language: string = "en", word: string): Promise<boolean> => {
+    const wordData: WordData | undefined =  await getWordData(language, word)
+    if (wordData == undefined || wordData.entries.length == 0) {
+        return false
+    } else {
+        return true
+    }
+}
+
+export const getWordData = async (language: string = "en", word: string): Promise<WordData | undefined> => {
     try {
         const response = await fetch(getURL(language, word))
         if (!response.ok) { 
             return undefined
         }
 
-        const data = await response.json()
+        const data: WordData = await response.json()
         return data
     } catch (e) {
         console.error(e)
